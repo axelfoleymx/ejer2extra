ff0ba483cc8c21f37e045933a2619eabb86e2659
¨axelfoleymx¨ <a01164173@gmail.com>
2016-08-15 14:40:46 -0500

    first commit

---
+++ config/initializers/assets.rb
@@ -0,0 +1,22 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( icomoon.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( magnific-popup.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( jquery.easing.1.3.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery.waypoints.min.js )
+Rails.application.config.assets.precompile += %w( jquery.magnific-popup.min.js )
+Rails.application.config.assets.precompile += %w( modernizr-2.6.2.min.js )
+Rails.application.config.assets.precompile += %w( main.js )
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
